3f90b09b918b0c3997a3406da31d6708115fe87b
Marius Kubilius <marius.kubilius@gmail.com>
2011-12-08 21:47:27 +0200

    Adding simple model with explanation how to query the model.

--- extensions/adapter/data/source/http/gMapApi.php
+++ extensions/adapter/data/source/http/gMapApi.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace gmapapi\extensions\adapter\data\source\http;
+namespace li3_gMapApi\extensions\adapter\data\source\http;
 
 use lithium\util\String;
 use lithium\util\Inflector;
@@ -38,7 +38,6 @@ class gMapApi extends \lithium\data\source\Http {
 			'places' => '/place/'.$this->_config['output'],
 		);
 		$params = $query->export($this, array('source', 'conditions'));
-		var_dump($query->model());
 		$source = $params['source'];
 		$conditions = (array) $params['conditions'] + array(
 			'sensor' => $this->_config['sensor'], 
@@ -50,7 +49,7 @@ class gMapApi extends \lithium\data\source\Http {
 		
 		$path = $paths[$source];
 		$result = json_decode($this->connection->get($this->_config['basePath'].$path, $conditions), true);
-		return $this->item($query->model(), $result['results'], array('class' => 'set'));
+		return $this->item($query->model(), $result, array('class' => 'set'));
 	}
 	
 	public function cast($entity, array $data, array $options = array()) {
---
+++ models/Geocodes.php
@@ -0,0 +1,9 @@
+<?php
+namespace li3_gMapApi\models;
+
+class Geocodes extends  \lithium\data\Model{
+	
+	public $_meta = array('connection' => 'gmapapi');
+	
+}
+?>
