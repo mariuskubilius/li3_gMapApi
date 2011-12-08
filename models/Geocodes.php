3cc905f5dfc183117535e2e45e75ac5587e3c228
Marius Kubilius <marius.kubilius@gmail.com>
2011-12-08 21:55:58 +0200

    Adding documentation on using models. Will add more after refactoring I think

--- models/Geocodes.php
+++ models/Geocodes.php
@@ -2,7 +2,17 @@
 namespace li3_gMapApi\models;
 
 class Geocodes extends  \lithium\data\Model{
-	
+	/**
+	 *  Usage example for Geocoding
+	 * 	us as you would use any model
+	 * 	for info on acceptable conditions please take a peak at:
+	 * 	http://code.google.com/apis/maps/documentation/webservices/index.html
+	 * 	
+	 * 	$conditions = array(
+	 * 		'address' => 'Chicago, USA',
+	 * 	);
+	 * 	Geocodes::all(compact('conditions'))
+	 */
 	public $_meta = array('connection' => 'gmapapi');
 	
 }
